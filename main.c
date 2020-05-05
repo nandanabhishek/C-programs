0aceb3c39089c097e2054c7e829577cf3e2136a4
nandan_abhishek <54950560+nandanabhishek@users.noreply.github.com>
2020-05-05 14:17:41 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float celsius;
+    float fahrenheit;
+
+    printf("Enter the value of temperature in celsius ");
+    scanf("%f", &celsius);
+    fahrenheit = (1.8) * celsius + 32;
+    printf("%.2f celsius=%.2f fahrenheit\n",celsius,fahrenheit);
+    return 0;
+
+}
