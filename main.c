8e4bc75ef6f08314e07d06184265025e6bc9143d
nandan_abhishek <54950560+nandanabhishek@users.noreply.github.com>
2020-05-05 21:52:41 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n1,n2,n3;
+    printf("Enter any 3 integers :");
+    scanf("%d%d%d",&n1,&n2,&n3);
+    printf("Largest integer is ");
+    if (n1 > n2)
+    {
+        if (n1 > n3)
+            printf("%d", n1);
+        else
+            printf("%d", n3);
+    }
+    else if(n2>n3)
+    {
+        if (n2 > n1)
+            printf("%d", n2);
+        else
+            printf("%d", n1);
+    }
+    else if(n3>n1)
+    {
+        if (n3 > n2)
+            printf("%d", n3);
+        else
+            printf("%d", n2);
+    }
+
+}
