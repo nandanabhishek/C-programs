d1ba30e33f23661efc6b1449c3e068f3f1ac8766
nandan_abhishek <54950560+nandanabhishek@users.noreply.github.com>
2020-05-08 15:01:36 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float p,q;
+    int z;
+    printf("enter any two numbers: ");
+    scanf("%f%f",&p,&q);
+    printf("Enter the option no which you want to evaluate:\n1.addition\n2.sub\n3.multi\n4.div \n");
+    scanf("%d",&z);
+    switch (z)
+        {
+            case 1:
+                printf("add=%f", p + q);
+                break;
+            case 2:
+                printf("sub=%f",p-q);
+                break;
+            case 3:
+                printf("mul=%f",p*q);
+                break;
+            case 4:
+                printf("div=%f",p/q);
+                break;
+            default:
+                printf("invalid option chosen");
+                break;
+        }
+
+
+
+}
