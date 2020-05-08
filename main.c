e8372a3ccfe8bf5fd516f728e8dffd1857d8a7e2
Abhishek Nandan <54950560+nandanabhishek@users.noreply.github.com>
2020-05-08 20:53:00 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,i,a,rev=0;
+    printf("enter the no to reverse it: ");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        a=n%10;
+        rev=rev*10+a;
+        n=n/10;
+    }
+    printf("reversed no = %d",rev);
+
+}
