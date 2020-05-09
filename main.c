db1a3eb07495fa9beda01a8ab5aa3b16aa6a1f4c
Abhishek Nandan <54950560+nandanabhishek@users.noreply.github.com>
2020-05-09 14:54:51 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int num, originalNum, remainder, result = 0;
+    printf("Enter a three-digit integer: ");
+    scanf("%d", &num);
+    originalNum = num;
+
+    while (originalNum != 0)
+    {
+        remainder = originalNum % 10;
+        result += pow(remainder,3);
+        originalNum /= 10;
+    }
+
+    if (result == num)
+        printf("%d is an Armstrong number.", num);
+    else
+        printf("%d is not an Armstrong number.", num);
+
+    return 0;
+}
