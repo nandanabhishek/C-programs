30060c2c595e07c1d286283744f94d9690d39bb2
Abhishek Nandan <54950560+nandanabhishek@users.noreply.github.com>
2020-05-11 17:39:34 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int max(int x, int y, int z);
+int main()
+{
+    int a,b,c;
+    printf("enter any three integers to compare them :");
+    scanf("%d %d %d",&a,&b,&c);
+    int p=max(a,b,c);
+    printf("%d is maximum",p);
+    return 0;
+
+}
+int max(int x, int y, int z)
+{
+    if(x>y && x>z)
+        return x;
+    else if(y>x && y>z)
+        return y;
+    else
+        return z;
+
+}
