e007adfad7335728edf5ee090cec0f5d89354523
Abhishek Nandan <54950560+nandanabhishek@users.noreply.github.com>
2020-05-12 23:02:38 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+float sq_sqrt(float x);
+
+int main()
+{
+    float a;
+    printf("Enter a no to find its square and square root: ");
+    scanf("%f",&a);
+    sq_sqrt(a);
+
+}
+float sq_sqrt(float x)
+{
+    float sq,sq_rt;
+    sq=pow(x,2);
+    sq_rt=sqrt(x);
+    printf("square and sq root of %f : %f and %f",x,sq,sq_rt);
+
+}
+
