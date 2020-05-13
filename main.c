687148dd7ca467421d1a060a99498f786ce5cab7
Abhishek Nandan <54950560+nandanabhishek@users.noreply.github.com>
2020-05-13 23:05:07 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void swap(int*, int*);
+int main()
+{
+    int x, y;
+
+    printf("Enter the value of x and y\n");
+    scanf("%d%d",&x,&y);
+    printf("Before Swapping\nx = %d\ny = %d\n", x, y);
+
+    swap(&x, &y);
+
+    printf("After Swapping\nx = %d\ny = %d\n", x, y);
+    return 0;
+}
+
+void swap(int *a, int *b)
+{
+    int t;
+
+    t  = *b;
+    *b = *a;
+    *a = t;
+}
