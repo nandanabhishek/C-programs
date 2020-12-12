38a58bd62b847fbe9e101fde585a8d83734a87e5
Abhishek Nandan <abhisheknandan00@gmail.com>
2020-12-13 00:49:13 +0530

    Create even_odd.py

---
+++ Checking Even or Odd/even_odd.py
@@ -0,0 +1,6 @@
+a = int(input(" Enter any number to check whether it is even or odd : "))
+if (a%2 == 0) :
+    print(a, "is Even !")
+    # this syntax inside print statement, automatically adds a space between data separated by comma
+else :
+    print(a, "is Odd !")
